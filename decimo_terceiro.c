5724c829aee7ba61fdd0482caf40215b104550f3
KayVasconcelos <84106035+KayVasconcelos@users.noreply.github.com>
2022-12-11 15:47:44 -0300

    criação das structs

--- decimo_terceiro.c
+++ decimo_terceiro.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//estrutura para armezenar as informações do pagamento do mês
+typedef struct{
+    float salario;
+    float comissao;
+} mespg;
+
+//estrutura para armezenar as informações de funcionário
+typedef struct{
+    char nome[100];
+    char cargo[50];
+    mespg pagamentos[12]; //vetor com 12 posições (12 meses) de pagamentos
+} funcionario;
+
